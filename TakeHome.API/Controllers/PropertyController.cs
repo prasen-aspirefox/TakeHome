cc418690374dbdf78dbc12925b6dd7b3b8505a55
Nikhil <nikhil.aspirefox@gmail.com>
2025-08-21 11:06:02 +0530

    Add project files.

---
+++ TakeHome.API/Controllers/PropertyController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TakeHome.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PropertyController : ControllerBase
+    {
+    }
+}
---
+++ TakeHome.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace TakeHome.Infrastructure.Data
+{
+    // Infrastructure/Data/AppDbContext.cs
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+
+        // Optional Fluent API config
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+        }
+    }
+
+}
